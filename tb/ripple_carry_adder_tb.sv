0136a58cccbdbfaab3aad169b459ecb6b4edeaf7
Armin-Pourmo <pourmohammadi.armin@gmail.com>
2026-06-23 11:56:06 -0700

    added ripple carry adder and its test bench

--- tb/ripple_carry_adder_tb.sv
+++ tb/ripple_carry_adder_tb.sv
@@ -1,54 +1,40 @@
 module ripple_carry_adder_tb;
 localparam WIDTH = 32;
 
-logic [WIDTH - 1:0] a ,b, sum, exp_sum;
-logic cin, cout,exp_cout;
+logic [WIDTH - 1:0] a ,b, sum;
+logic [WIDTH:0] exp_sum;
+logic cin, cout;
 
-ripple_carry_adder #(.WIDTH(WIDTH)) dut(a,b,cin,sum,cout);
+ripple_carry_adder #(.WIDTH(WIDTH)) dut(
+    .a(a),
+    .b(b),
+    .sum(sum),
+    .cout(cout),
+    .cin(cin)
+    );
 
-integer fh;
-integer scan_result;
-integer pass_count, fail_count, test_num;
+task check();
+    exp_sum = a + b + cin;
+    #5;
+    if ({cout,sum} !== exp_sum)
+        $error("FAIL a:%h | b:%h | cin:%b | sum:%h | exp_sum: %h", a,b,cin,sum,exp_sum[31:0]);
+    else
+        $display("PASS a:%h | b:%h | cin:%b | sum:%h | exp_sum: %h", a,b,cin,sum,exp_sum[31:0]);
+endtask
 
 initial begin
+    $dumpfile("dump/ripple_dump.vcd");
+    $dumpvars(0, ripple_carry_adder_tb);
+    a = 32'h00000000; b = 32'h00000000; cin = 1'b0; #10; check();
+    a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; cin = 1'b0; #10; check();
+    a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; cin = 1'b1; #10; check();
 
-  $dumpfile("dump/ripple_dump.vcd");
-  $dumpvars(0, ripple_carry_adder_tb);
-
-  // rest of code
-    pass_count = 0;
-    fail_count = 0;
-    test_num = 0;
-
-    fh = $fopen("tb/ripple_carry_adder_tb.tv","r");
-    if (fh==0)begin
-        $display("ERROR: could not open vector file");
-        $finish;
+    for(int i = 0; i < 50; i++) begin
+        a = $urandom;
+        b = $urandom;
+        cin = $urandom_range(0, 1);
+        #10;
+        check();
     end
-    begin : read_loop
-        string line;
-        while (!$feof(fh)) begin
-            if ($fgets(line, fh) == 0) break;
-            if (line.substr(0,1) == "//") continue;
-            scan_result = $sscanf(line, "%b %b %b %b %b", a, b, cin, exp_sum, exp_cout);
-            if (scan_result != 5) continue;
-            #10;
-
-            test_num++;
-            if(sum !== exp_sum || cout !== exp_cout) begin
-                $display("FAIL test %0d | a=%b b=%b cin=%b | got sum=%b cout=%b | expected sum=%b cout=%b",
-                            test_num,a,b,cin,sum,cout,exp_sum,exp_cout);
-                fail_count++;
-            end else begin
-                $display("PASS test %0d | a=%b b=%b cin=%b = %b",
-                            test_num,a,b,cin,sum);
-                pass_count++;
-            end
-        end
-    end : read_loop
-    $fclose(fh);
-    $display("--- %0d passed, %0d failed ---", pass_count, fail_count);
-    $finish;
 end
-
 endmodule
